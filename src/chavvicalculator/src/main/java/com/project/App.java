15bc995e672aef47152086a40c71a95c3cbea862
L00GLE <37436137+L00GLE@users.noreply.github.com>
2022-09-22 18:07:01 -0700

    Added display of Menu Options.

--- src/chavvicalculator/src/main/java/com/project/App.java
+++ src/chavvicalculator/src/main/java/com/project/App.java
@@ -18,5 +18,33 @@ public class App
         double result = 0.000;
 
         Scanner scan = new Scanner(System.in);
+
+        
+
+        // Program in loop
+        while (true) {
+
+
+            // Menu
+            System.out.println("");
+            System.out.println("Chavvi's Calcuator");
+            System.out.println("..............................");
+            System.out.println("a   Enter values for A");
+            System.out.println("b   Enter values for B");
+            System.out.println("+   Add");
+            System.out.println("-   Subtract");
+            System.out.println("*   Multiply");
+            System.out.println("/   Divide");
+            System.out.println("c   Clear");
+            System.out.println("q   Quit");
+            System.out.println("..............................");
+            System.out.println("A = " + num1 + "    B = " + num2);
+            System.out.println("..............................");
+            System.out.print("Enter a function: ");
+    
+    
+            function = scan.next().charAt(0);
+        }
+    }
     }
 }
